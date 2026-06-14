129b2cbd5ee4ed300258e6e86ab8e97f8f229c5c
Siri Narne <sirinarne@mac.mynetworksettings.com>
2026-06-14 19:54:01 -0400

    Completed exploratory data analysis

---
+++ src/data_cleaning.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/sirinarne/Downloads/Projects/College_Major_ROI_Analysis/data/raw/recent-grads.csv")
+print(df.columns)
+print(df.shape)
+print(df.head())
